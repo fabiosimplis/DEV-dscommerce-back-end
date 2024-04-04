14abfd96d81a45de0cc1089a17a85e7a5005c7b9
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-04-04 20:21:58 -0300

    ADD Consulta de produtos por nome

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -20,8 +20,10 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> getProducts(Pageable pageable){
-        Page<ProductDTO> dto = service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> getProducts(
+            @RequestParam(name = "name", defaultValue = "") String name,
+            Pageable pageable){
+        Page<ProductDTO> dto = service.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
     @GetMapping(value = "/{id}")
--- src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/ProductRepository.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dscommerce.repositories;
 
 import com.devsuperior.dscommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
+    @Query("SELECT p FROM Product p WHERE UPPER(p.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+    Page<Product> searchByName(String name, Pageable pageable);
 }
