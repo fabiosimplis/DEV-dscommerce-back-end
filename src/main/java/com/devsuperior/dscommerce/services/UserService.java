73a818c41c0a83857c9081e80c1f9c6f77559895
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-04-07 17:02:58 -0300

    ADD UserDetailsService

---
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.entities.Role;
+import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.projection.UserDetailsProjection;
+import com.devsuperior.dscommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+
+        if (result.isEmpty()){
+            throw new UsernameNotFoundException("User Not Found");
+        }
+
+        User user = new User();
+        user.setEmail(username);
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection udp : result){
+            user.addRole(new Role(udp.getRoleId(), udp.getAuthority()));
+        }
+
+        return user;
+    }
+}
