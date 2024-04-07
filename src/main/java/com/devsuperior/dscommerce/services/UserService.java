04028c842e5ee338e2b1f98923f6cabe583c53db
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-04-07 18:41:10 -0300

    ADD GET user logged

--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,14 +1,19 @@
 package com.devsuperior.dscommerce.services;
 
+import com.devsuperior.dscommerce.dto.UserDTO;
 import com.devsuperior.dscommerce.entities.Role;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projection.UserDetailsProjection;
 import com.devsuperior.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -35,5 +40,28 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    protected User getUserAuthenticated(){
+
+        try {
+            //Pega objeto dentro do contexto Spring security
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            //Obtem o claim
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String userName = jwtPrincipal.getClaimAsString("username");
+
+            return repository.findByEmail(userName).get();
+        }
+        catch (Exception e){
+            throw new UsernameNotFoundException("Email Not Found");
+        }
+
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getLoggedUser(){
+        User user = getUserAuthenticated();
+        return new UserDTO(user);
     }
 }
