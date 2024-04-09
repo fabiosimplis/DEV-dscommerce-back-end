7c280f78d2638e45e92d5f99637485ec48be0d13
fabio.simplis <31253667+fabiosimplis@users.noreply.github.com>
2024-04-09 19:49:14 -0300

    ADD salvando o pedido(Order)

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -3,27 +3,27 @@ package com.devsuperior.dscommerce.dto;
 import com.devsuperior.dscommerce.entities.OrderItem;
 
 public class OrderItemDTO {
-    private Long productID;
+    private Long productId;
     private String name;
     private Double price;
     private Integer quantity;
 
     public OrderItemDTO(Long productID, String nome, Double price, Integer quantity) {
-        this.productID = productID;
+        this.productId = productID;
         this.name = nome;
         this.price = price;
         this.quantity = quantity;
     }
 
     public OrderItemDTO(OrderItem entity) {
-        productID = entity.getProduct().getId();
+        productId = entity.getProduct().getId();
         name = entity.getProduct().getName();
         price = entity.getPrice();
         quantity = entity.getQuantity();
     }
 
-    public Long getProductID() {
-        return productID;
+    public Long getProductId() {
+        return productId;
     }
 
     public String getName() {
